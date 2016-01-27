60b62643c6dcac025bac102648771a712a8aa2d1
Psycho killer <fucked@psychokiller.me>
2016-01-28 00:50:00 +0800

    init

---
+++ app/modules/components/user_withdraw/user_withdraw.js
@@ -0,0 +1,14 @@
+// 提现
+var button = require('../btn/btn.js');
+
+if ($('.user_withdraw').length) {
+  $('.container').css('height',$('html').offset().height);
+
+  $('button').on('click',function(e) {
+
+
+    setTimeout(function(){
+
+    },1000);
+  });
+}
