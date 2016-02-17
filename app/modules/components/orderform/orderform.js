3313cb5e1d8143d190d7cd61310fb12ef647d987
psychokiller666 <fucked@psychokiller.me>
2016-02-17 11:05:02 +0800

    添加确认支付页面

--- app/modules/components/orderform/orderform.js
+++ app/modules/components/orderform/orderform.js
@@ -1,23 +1,33 @@
-// 生成订单数量操作
-var add_btn = $('#add');
-var min_btn = $('#min');
-var order_number = $('#number');
+// 微信jssdk
+var wx = require('weixin-js-sdk');
 
-//初始化数量为1,并失效减
-order_number.val(1);
-min_btn.attr('disabled',true);
-//数量增加操作
-add_btn.tap(function(){
-  order_number.val(parseInt(order_number.val())+1)
-  if (parseInt(order_number.val())!=1){
-    min_btn.attr('disabled',false);
-  }
-})
-//数量减少操作
-min_btn.tap(function(){
-  if (parseInt(order_number.val())==1){
-    min_btn.attr('disabled',true);
-  } else {
-    order_number.val(parseInt(order_number.val())-1);
-  }
-})
+if($('.orderform').length) {
+  // 生成订单数量操作
+  var add_btn = $('#add');
+  var min_btn = $('#min');
+  var order_number = $('#number');
+
+  //初始化数量为1,并失效减
+  order_number.val(1);
+  min_btn.attr('disabled',true);
+  //数量增加操作
+  add_btn.tap(function(){
+    order_number.val(parseInt(order_number.val())+1)
+    if (parseInt(order_number.val())!=1){
+      min_btn.attr('disabled',false);
+    }
+  })
+  //数量减少操作
+  min_btn.tap(function(){
+    if (parseInt(order_number.val())==1){
+      min_btn.attr('disabled',true);
+    } else {
+      order_number.val(parseInt(order_number.val())-1);
+    }
+  })
+
+  // 添加地址
+  $('.addr').tap(function(){
+    console.log('sds');
+  });
+}
