2e5f73bc11e5045b4b04b9d30ac3e0f98043f42d
psychokiller666 <fucked@psychokiller.me>
2016-02-18 20:08:49 +0800

    完成80%评论弹出交互

--- app/modules/components/dialog_comment/dialog_comment.js
+++ app/modules/components/dialog_comment/dialog_comment.js
@@ -5,40 +5,47 @@ var dialog_comment = $('.dialog_comment');
 var father_comment = $('.father');
 var son_comment = $('.son');
 // 弹出回复框
-function comment_box(id,height) {
-  dialog_comment.addClass('hs-show').removeClass('hs-hide');
-  footer_nav.addClass('hs-hide').removeClass('hs-show');
-  // dialog_comment.parent('.hs-show-footer').addClass('input-fixfixed');
-
-
-  $('main').scrollTop(height);
+function comment_box(id,username) {
+  dialog_comment.dialog("show");
+  if (username) {
+    $('#comment_input').attr('placeholder','回复：'+username);
+  }
   $('#comment_input').focus();
-  // console.log(height);
+  // 禁止滑动
+  dialog_comment.on('touchmove',function(e){
+    e.stopPropagation();
+  });
+  // 控制关闭
+  dialog_comment.find('.cancel').on('click',function(){
+    dialog_comment.dialog("hide");
+  });
+  // 提交评论
+  dialog_comment.find('.submit').on('click',function(){
+    dialog_comment.find('button').attr('disabled','disabled');
+  });
+
+  // 上传图片
+
 }
 
+
+// 点击写评论
 comment_btn.on('click',function(e){
   $(this).addClass('active');
-  comment_box();
-});
-
-// dialog_comment.find('input').on('focus', function(e) {
-//   dialog_comment.parent('.hs-show-footer').addClass('input-fixfixed');
-// });
-
-dialog_comment.find('input').on('blur', function(e) {
-  comment_btn.removeClass('active');
-  dialog_comment.addClass('hs-hide').removeClass('hs-show');
-  footer_nav.addClass('hs-show').removeClass('hs-hide');
+  comment_box('a');
 });
 
 
+// 点击评论框
 father_comment.on('click',function(){
   var comment_id = $(this).data('id');
-  comment_box(comment_id,$(this).find('.username').offset().top);
-  console.log($(this).find('.username').offset().top);
-})
-son_comment.on('click',function(){
-  var comment_id = $(this).data('id');
-  comment_box(comment_id,$(this).offset().top);
+  var username = $(this).find('.username .hs-fl').text();
+  comment_box(comment_id,username);
 })
 
+// 点击回复框
+son_comment.on('click',function(){
+  var comment_id = $(this).data('id');
+  var username = $(this).find('.span').text();
+  comment_box(comment_id,username);
+})
