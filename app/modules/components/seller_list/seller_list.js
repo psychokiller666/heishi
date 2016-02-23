8e732ad90382078c3c76d1e3181d5b2a7119cdbe
Psycho killer <fucked@psychokiller.me>
2016-02-23 23:12:15 +0800

    完善卖家动态页交互

---
+++ app/modules/components/seller_list/seller_list.js
@@ -0,0 +1,19 @@
+var dropload = require('../../../../bower_components/dropload/dist/dropload.min.js');
+
+if($('.seller_list').length){
+  var seller_list_bd = $('.seller_list_bd');
+
+  seller_list_bd.dropload({
+    domDown : {
+      domClass : 'dropload-down',
+      domRefresh: '<div class="dropload-refresh">🌚 往上拉。</div>',
+      domLoad : '<div class="dropload-load">😏 加载呢。</div>',
+      domNoData : '<div class="dropload-noData">😢 没有咯。</div>'
+    },
+    scrollArea : seller_list_bd,
+    loadDownFn : function(e){
+      e.noData();
+      e.resetload();
+    }
+  })
+}
