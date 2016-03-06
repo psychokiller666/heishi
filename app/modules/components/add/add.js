9888ecdb4726765063220b40505fcd01dc242556
Psycho killer <fucked@psychokiller.me>
2016-03-07 01:02:56 +0800

    周末在家没做完

--- app/modules/components/add/add.js
+++ app/modules/components/add/add.js
@@ -1,6 +1,45 @@
 // 发布页
+// 页面初始化
+var common = require('../common/common.js');
+// 百度上传组件
+var WebUploader = require('../../../../node_modules/tb-webuploader/dist/webuploader.min.js');
 
 $(document).on('pageInit','.add', function (e, id, page) {
+  if (page.selector == '.page'){
+    return false;
+  }
+  var init = new common(page);
+  init.wx_share(false);
+  // 上传图片
+  var images = $('.images');
+
+  var uploader = WebUploader.create({
+    fileNumLimit: 16,
+    // 自动上传。
+    auto: true,
+    // 文件接收服务端。
+    server: '/index.php?g=api&m=HsFileupload&a=upload',
+    // 二进制上传
+    sendAsBinary: true,
+    // 只允许选择文件，可选。
+    accept: {
+      title: 'Images',
+      extensions: 'gif,jpg,jpeg,bmp,png',
+      mimeTypes: 'image/*'
+    }
+  });
+  // 监听input file是否有文件添加进来
+  images.on('change','.webuploader-element-invisible', function(e) {
+    console.log(e.target.files);
+    console.log(e);
+    if($(this).offsetParent().className == 'cover') {
+      console.log('头图');
+    } else {
+      console.log('不是头图');
+    }
+    uploader.addFiles(e.target.files);
+  });
+
   // 选择标签
   var tags = $('.tags');
   var tags_num = tags.find('button.active').length;
--- app/modules/components/already_list/already_list.js
+++ app/modules/components/already_list/already_list.js
@@ -1,10 +1,13 @@
 // 页面初始化
 var common = require('../common/common.js');
 
-$(document).on('pageInit','.bought', function (e, id, page) {
+$(document).on('pageInit','.already_list', function (e, id, page) {
+  console.log('dssddsds');
   if (page.selector == '.page'){
     return false;
   }
   var init = new common(page);
   init.wx_share(false);
+  console.log('dssddsds');
+
 })
