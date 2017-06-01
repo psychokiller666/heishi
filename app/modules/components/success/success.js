38458e765698fdf37407ccba3d7027913ba6201d
lxf1002273683 <1002273683@qq.com>
2017-06-01 17:07:37 +0800

    支付成功跳转 活动

--- app/modules/components/success/success.js
+++ app/modules/components/success/success.js
@@ -8,8 +8,9 @@ $(document).on('pageInit','.jump', function(e, id, page){
   }
   var init = new common(page);
   init.wx_share(false);
-
-  if($('.success').length){
+  var status_pay = $('.status_pay').val();
+  //支付成功
+  if($('.success').length && status_pay == 0){
     $.showPreloader('确认支付中');
     init.cnzz_push('完成订单',{
       '订单ID': $(page).data('ordernumber'),
@@ -29,7 +30,7 @@ $(document).on('pageInit','.jump', function(e, id, page){
       window.location.href = $('.no_data').data('url');
     },3000);
   }
-
+  //支付错误
   if($('.error').length) {
     if($('.no_data').attr('data-url')){
       setTimeout(function(){
@@ -43,4 +44,18 @@ $(document).on('pageInit','.jump', function(e, id, page){
 
   }
 
+  //活动 支付成功
+  if(status_pay == 1){
+    $.hidePreloader();
+    $.ajax({
+      type: "GET",
+      url: '/index.php?g=api&m=HsDigitalGoods&a=generate_digital_code&order_number=' + $(page).data('ordernumber'),
+      success: function(data){
+        $('.no_data').text(2);
+        var digital = JSON.parse(data);
+        var str = '<div><p>爸爸</p><p>您已启动黑市秘籍</p><p>游戏过后可直接观看成功视频</p></div><a href="http://amuseddie.com/HeiShi/?hcode='+digital.data+'">前往体验</a>'
+        $('.no_data').html(str);
+      }
+    })
+  }
 });
