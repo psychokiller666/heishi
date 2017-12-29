24ab7e13feb4ff0a750119345684818a39b72167
lxf1002273683 <1002273683@qq.com>
2017-12-29 10:39:27 +0800

    删除黄道忌日活动跳转

--- app/modules/components/success/success.js
+++ app/modules/components/success/success.js
@@ -10,7 +10,8 @@ $(document).on('pageInit','.jump', function(e, id, page){
   init.wx_share(false);
   var status_pay = $('.status_pay').val();
   //支付成功
-  if($('.success').length && status_pay == 0){
+  // if($('.success').length && status_pay == 0){
+  if($('.success').length){
     $.showPreloader('确认支付中');
     setTimeout(function(){
       $.hidePreloader();
@@ -35,17 +36,17 @@ $(document).on('pageInit','.jump', function(e, id, page){
   }
 
   //活动 支付成功
-  if(status_pay == 1){
-    $.hidePreloader();
-    $.ajax({
-      type: "GET",
-      url: '/index.php?g=api&m=HsDigitalGoods&a=generate_digital_code&order_number=' + $(page).data('ordernumber'),
-      success: function(data){
-        $('.no_data').text(2);
-        var digital = JSON.parse(data);
-        var str = '<div><p>爸爸</p><p>您已启动黑市秘籍</p><p>游戏过后可直接观看成功视频</p></div><a href="//amuseddie.com/HeiShi/?hcode='+digital.data+'">前往体验</a>'
-        $('.no_data').html(str);
-      }
-    })
-  }
+  // if(status_pay == 1){
+  //   $.hidePreloader();
+  //   $.ajax({
+  //     type: "GET",
+  //     url: '/index.php?g=api&m=HsDigitalGoods&a=generate_digital_code&order_number=' + $(page).data('ordernumber'),
+  //     success: function(data){
+  //       $('.no_data').text(2);
+  //       var digital = JSON.parse(data);
+  //       var str = '<div><p>爸爸</p><p>您已启动黑市秘籍</p><p>游戏过后可直接观看成功视频</p></div><a href="//amuseddie.com/HeiShi/?hcode='+digital.data+'">前往体验</a>'
+  //       $('.no_data').html(str);
+  //     }
+  //   })
+  // }
 });
