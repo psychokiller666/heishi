07289ebb0905af0e9994f7da947783247a0a5a41
皓然 <3151581688@qq.com>
2018-12-18 18:15:24 +0800

    [大专题修改页面跳转方式]圣诞活动大改

--- app/modules/components/activity/christmas.js
+++ app/modules/components/activity/christmas.js
@@ -2,7 +2,7 @@
 
 // 初始化
 var common = require('../common/common.js');
-
+var velocity = require('../plugin/velocity.min.js');
 $(document).on('pageInit','.christmas', function(e, id, page) {
     if (page.selector == '.page') {
         return false;
@@ -22,12 +22,14 @@ $(document).on('pageInit','.christmas', function(e, id, page) {
 
     // 调用微信分享sdk
     var share_data = {
-        title: '公路商店 — 新人礼遇',//todo:
+        title: '公路商店',//todo:
         desc: '为你不着边际的企图心',
         link: window.location.href,
         img: 'https://jscache.ontheroadstore.com/tpl/simplebootx_mobile/Public/i/logo.png'
     };
     init.wx_share(share_data);
+
+    // $('body,.page, .page-group').css('background','#9c0404');
 
 
     //判断是否是app
@@ -35,11 +37,13 @@ $(document).on('pageInit','.christmas', function(e, id, page) {
     var $isApp = $('.is_app');
     var Authorization = $isApp.attr('authorization');
     var uid = $isApp.attr('uid');
+    var deviceType = $isApp.attr('device_type');//值：IOS ANDROID WECHAT NO
     // var version = $isApp.attr('version');
 
     var loginStatus = true;
 
     if(Authorization && Authorization.length>0){
+        //有Authorization值肯定是登录的状态
         isApp = true;
         ajaxHeaders = {
             'Authorization' : Authorization,
@@ -52,17 +56,61 @@ $(document).on('pageInit','.christmas', function(e, id, page) {
 
 
 
+    if(deviceType==='WECHAT' || deviceType==='NO'){
+        $(page).find('.return_home_page').show();
+    }
 
-    $(page).on('click','.get_coupon',function(){
 
-        if(isApp){
-            if(!uid){
+    var $act_cut_line = $(page).find('.act_cut_line');
+
+    var $content = $(page).find('.content');
+
+    var $popup_wrap = $(page).find('.popup_wrap');
+    $popup_wrap.on('click','.popup_btn',function(){
+        if($(this).hasClass('popup_yes')){
+            //滚动到指定位置
+            var scrollTop = $content[0].scrollTop;//当前位置
+            var offsetTop = $act_cut_line.offset().top; //得到box这个div层的offset，包含两个值，top和left
+            // $content.scrollTop(scrollTop + offsetTop)
+            start(scrollTop,offsetTop,50,function(top){
+                $content.scrollTop(top);
+            })
+        }
+        hidePopup();
+    });
+    $popup_wrap.on('click','.popup_mask',function(){
+        hidePopup();
+    });
+
+    function showPopup(){
+        $popup_wrap.show();
+    }
+    function hidePopup(){
+        $popup_wrap.hide();
+    }
+
+    //t当前时间，b初始值，c变化量，d持续时间
+    function linear(t,b,c,d){ return c*t/d + b; }
+    function start(b,c,d,callback){
+        var t=0;
+        function run(){
+            var now = Math.ceil(linear(t,b,c,d));
+            callback(now);
+            if(t<d){ t++; setTimeout(run, 1); }
+        }
+        run();
+    }
+
+    $(page).on('click','.act_coupon_li',function(){
+
+        if(!loginStatus){
+            if(deviceType==='WECHAT' || deviceType==='NO'){
+                if(init.ifLogin(true) == false){
+                    return ;
+                }
+            }else{
                 $.toast('请先登录');
                 return;
-            }
-        }else{
-            if(init.ifLogin(true) == false){
-                return ;
             }
         }
 
@@ -83,25 +131,60 @@ $(document).on('pageInit','.christmas', function(e, id, page) {
             url: url,
             dataType: 'json',
             data: {},
-            headers: {
-                'phpsessionid': PHPSESSID
-            },
+            headers: ajaxHeaders,
 
             success: function(data){
                 if(data.status==1){
-                    $.toast('领取成功');//todo:
+                    showPopup();
                 }else{
-                    $.toast(data.info);
+                    var info = data.info;
+                    if(str_length(info)>15){
+                        info = cut_str(info,15) + '…';
+                    }
+                    $.toast(info);
                 }
                 $btn.attr('clicked','0');
             },
-            error: function(e){
+            error: function(xhr){
                 $btn.attr('clicked','0');
-                $.toast('领取失败,请稍后重试');
+                if(xhr.status==0){
+                    if(deviceType==='WECHAT' || deviceType==='NO'){
+                        if(init.ifLogin(true) == false){
+                            return ;
+                        }
+                    }else{
+                        $.toast('页面已过期');
+                        return;
+                    }
+                }
+                $.toast('领取失败,请刷新后重试');
                 console.log('getACoupon err: ',e);
             }
 
         });
+    }
+
+
+
+    //获取字符串长度
+    function str_length(s){
+        if(typeof s === "string"){
+            return s.replace(/[\u0391-\uFFE5]/g,"aa").length;
+        }
+        return 0;
+    }
+    //截取 n个汉字，一个汉字等于2字符
+    function cut_str(str, len){
+        var char_length = 0;
+        for (var i = 0; i < str.length; i++){
+            var son_str = str.charAt(i);
+            encodeURI(son_str).length > 2 ? char_length += 1 : char_length += 0.5;
+            if (char_length >= len){
+                var sub_len = char_length == len ? i+1 : i;
+                return str.substr(0, sub_len);
+                break;
+            }
+        }
     }
 
 
