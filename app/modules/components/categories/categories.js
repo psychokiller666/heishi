97b233cd4c815f6db8eb84c7ef0d0c2bc2393092
zjimmyking <zjimmyking@163.com>
2019-11-05 10:25:41 +0800

    开发完成待测试

--- app/modules/components/categories/categories.js
+++ app/modules/components/categories/categories.js
@@ -5,29 +5,49 @@ var common = require('../common/common.js');
 // 搜索
 // var SearchInit = require('../search_list/search_list.js');
 
-$(document).on('pageInit','.categories', function(e, id, page){
-  if (page.selector == '.page'){
+$(document).on('pageInit', '.categories', function (e, id, page) {
+  if (page.selector == '.page') {
     return false;
   }
   var init = new common(page);
   init.checkfollow();
-
+  var ApiBaseUrl = init.getApiBaseUrl();
   // 搜索初始
   // SearchInit();
+  $.ajax({
+    url: ApiBaseUrl+'/appv3/categories?level=1&depth=1',
+    type: 'get',
+    success: function (res) {
+      if (res.code) {
+        initDate(res);
+        $('.classify_one li').click(function () {
+          var that = this;
+          let idx = $(that).index()
+          let secondsLevelHtml =''
+          $('.classify_one li').removeClass('active');
+          $(that).addClass('active');
+          secondsLevelHtml += freshSecondLevelCategory(res.data[idx]);
+          $('.classify_two').html(secondsLevelHtml);
 
+        })
+      } else {
+        $('.hs-page').html('<H1>网络不佳请稍后再试</H1>');
+      }
+    }
+  })
 
   // 初始化title
   $('title').text('分类');
   // 存储每个子分类位置
   var categoriesTitlePosition = {};
-  $('.categories_title').each(function(){
-    var class_name = 'categories_title'+$(this).attr('data-id');
+  $('.categories_title').each(function () {
+    var class_name = 'categories_title' + $(this).attr('data-id');
     categoriesTitlePosition[class_name] = $(this).position().top;
   })
-  $(".classify_two").on('scroll',function(){
-    $('.categories_title').each(function(){
+  $(".classify_two").on('scroll', function () {
+    $('.categories_title').each(function () {
       var top = Math.abs($(this).position().top);
-      if(top <= 100){
+      if (top <= 100) {
         var class_name = '.categories' + $(this).attr('data-id');
         $('.classify_one li').removeClass('active');
         $(class_name).addClass('active');
@@ -35,16 +55,16 @@ $(document).on('pageInit','.categories', function(e, id, page){
     })
   });
   // 子分类切换
-  $('.classify_one li').click(function(){
-    var that = this;
-    $('.classify_one li').removeClass('active');
-    $(that).addClass('active');
-    var class_name = 'categories_title' + $(that).attr('data-id');
-    $(".classify_two").scrollTop(categoriesTitlePosition[class_name]);
-  })
-  
+  // $('.classify_one li').click(function () {
+  //   var that = this;
+  //   $('.classify_one li').removeClass('active');
+  //   $(that).addClass('active');
+  //   var class_name = 'categories_title' + $(that).attr('data-id');
+  //   $(".classify_two").scrollTop(categoriesTitlePosition[class_name]);
+  // })
+
   // 分类标签切换
-  $('.label').click(function(){
+  $('.label').click(function () {
     $('.classify').removeClass('active');
     $('.label').addClass('active');
     $('.classify_content').css('display', 'none');
@@ -52,7 +72,7 @@ $(document).on('pageInit','.categories', function(e, id, page){
     $('.label_content').css('display', 'block');
     $('.bg').addClass('bg_right');
   })
-  $('.classify').click(function(){
+  $('.classify').click(function () {
     $('.label').removeClass('active');
     $('.classify').addClass('active');
     $('.classify_content').css('display', 'block');
@@ -61,47 +81,203 @@ $(document).on('pageInit','.categories', function(e, id, page){
     $('.bg').removeClass('bg_right');
   })
 
-    //  神策埋点事件
-    sensorsEvent();
-    function sensorsEvent() {
-        $(page).find('.search_btn').on('click',function(){
-            init.sensors.track('buttonClick', {
-                pageType : '分类页',
-                buttonName : '搜索',
-            })
-        });
-        init.sensorsFun.bottomNav();
-
-        //分类 分类列表页
-        $(page).find('.classify_two').on('click','a',function(){
-            var $this = $(this);
-            var $li = $this.parents('li');
-            var url = $this.attr('href');
-            var index = $li.index();
-            var title = $this.find('span').html();
-
-            init.sensorsFun.mkt('分类','分类列表页',title,index,'','');
-        });
-
-        //标签 标签列表页
-        $(page).find('.label_content .tags_list').on('click','a',function(){
-            var $this = $(this);
-            var $li = $this;
-            var url = $this.attr('href');
-            var index = $li.index();
-            var title = init.sensorsFun.getUrlKey(url,'tag');
-
-            init.sensorsFun.mkt('标签','标签列表页',title,index,'','');
-        });
-
-        //黑市书摊 标签列表页
-        $(page).find('.label_content').children('a').on('click',function(){
-            var $this = $(this);
-            var url = $this.attr('href');
-            init.sensorsFun.mkt('黑市书摊','标签列表页',url,'','','');
-        });
-
+  function initDate(res) {
+    //左侧一级的菜单
+    let fisrtLevelHtml = '';
+    //右边的二级菜单
+    let secondsLevelHtml = '';
+    for (let i = 0; i<res.data.length;i++) {
+     let  firstCategory = res.data[i];
+      fisrtLevelHtml += freshFirstLevelCategory(firstCategory, i);
+      if(i==0){
+        secondsLevelHtml += freshSecondLevelCategory(firstCategory);
+      }
+      
     }
+    $('ul[class="classify_one"]').html(fisrtLevelHtml);
+    $('.classify_two').html(secondsLevelHtml);
+
+
+    $('title').text('分类');
+    // 存储每个子分类位置
+    var categoriesTitlePosition = {};
+    $('.categories_title').each(function () {
+      var class_name = 'categories_title' + $(this).attr('data-id');
+      categoriesTitlePosition[class_name] = $(this).position().top;
+    })
+    $(".classify_two").on('scroll', function () {
+      $('.categories_title').each(function () {
+        var top = Math.abs($(this).position().top);
+        if (top <= 100) {
+          var class_name = '.categories' + $(this).attr('data-id');
+          $('.classify_one li').removeClass('active');
+          $(class_name).addClass('active');
+        }
+      })
+    })
+    // 子分类切换
+    $('.classify_one li').click(function () {
+      var that = this;
+      $('.classify_one li').removeClass('active');
+      $(that).addClass('active');
+      var class_name = 'categories_title' + $(that).attr('data-id');
+      $(".classify_two").scrollTop(categoriesTitlePosition[class_name]);
+    })
+
+    // 分类标签切换
+    $('.label').click(function () {
+      $('.classify').removeClass('active');
+      $('.label').addClass('active');
+      $('.classify_content').css('display', 'none');
+      $('.classify_two').css('display', 'none');
+      $('.label_content').css('display', 'block');
+      $('.bg').addClass('bg_right');
+    })
+    $('.classify').click(function () {
+      $('.label').removeClass('active');
+      $('.classify').addClass('active');
+      $('.classify_content').css('display', 'block');
+      $('.classify_two').css('display', 'block');
+      $('.label_content').css('display', 'none');
+      $('.bg').removeClass('bg_right');
+    })
+  }
+
+  function freshSecondLevelTitle(firstCategory) {
+    let url = genrateUrl(firstCategory.url, firstCategory.url_type);
+    let basicTitle = ''
+    return basicTitle =
+      '<a href="' + url + '"><div class="categories_title categories_title' + firstCategory.id + '" data-id="' + firstCategory.id + '">' +
+      '<div class="categories_bg" data-layzr="' + firstCategory.ad_image + '" data-layzrstatus="1" style="background-image: url(' + firstCategory.ad_image + ');"></div>' +
+      '<h4>' + firstCategory.name + '</h4>' +
+      '<div>' + firstCategory.desc + '</div>' +
+      '</div></a>'
+  }
+
+  function freshSecondLevelCategory(firstCategory) {
+    //处理那个大图片
+    let title = freshSecondLevelTitle(firstCategory)
+    //处理地下的小分类
+    let contents = freshSecondLevelContent(firstCategory);
+    return title + contents;
+  }
+  function freshFirstLevelCategory(firstCategory, num) {
+    if (num === 0 || num === '0') {
+      return '<li data-id="' + firstCategory.id + '" class="active categories categories' + firstCategory.id + '">' + firstCategory.name + '</li>';
+    } else {
+      return '<li data-id="' + firstCategory.id + '" class="categories categories' + firstCategory.id + '">' + firstCategory.name + '</li>';
+    }
+  }
+  function freshSecondLevelContent(firstCategory) {
+    let content = '';
+    if (typeof firstCategory.adList === 'object' && firstCategory.adList.length > 0) {
+      content += '<div class="tit">热门推荐</div>'
+      content = content + '<ul class="classify_two_list border_bot classify_two_list">';
+      for (let adListNum in firstCategory.adList) {
+        content = content + freashAdLi(firstCategory.adList[adListNum]);
+      }
+      content = content + '</ul>';
+    }
+    if (typeof firstCategory.children === 'object' && firstCategory.children.length > 0) {
+      content += '<div class="tit">精选分类</div>'
+      content = content + '<ul class="classify_two_list classify_two_list">';
+      for (let kidCategoryNum in firstCategory.children) {
+        content = content + freashKidLi(firstCategory.children[kidCategoryNum]);
+      }
+      content = content + '</ul>';
+    }
+
+    if (content === '') {
+      content = '<ul class="classify_two_list classify_two_list"></ul>'
+    }
+
+    return content
+  }
+
+
+  function freashKidLi(child) {
+    return '<li>' +
+      '       <a href="/HsCategories/category_index/id/' + child.id + '" class="external">' +
+      '          <div class="image" data-layzr="' + child.icon + '" data-layzrstatus="1" style="background-image: url(' + child.icon + ');"></div>' +
+      '          <span>' + child.name + '</span>' +
+      '       </a>' +
+      '   </li>';
+  }
+
+  function freashAdLi(adListElement) {
+    let url = genrateUrl(adListElement.url, adListElement.url_type);
+    return '<li>' +
+      '       <a href="' + url + '" class="external">' +
+      '          <div class="image" data-layzr="' + adListElement.img + '" data-layzrstatus="1" style="background-image: url(' + adListElement.img + ');"></div>' +
+      '          <span>' + adListElement.description + '</span>' +
+      '       </a>' +
+      '   </li>';
+  }
+
+  function genrateUrl(url, url_type) {
+    if (url_type === 0 || url_type === '0') {
+      return 'javascript:;';
+    }
+    if (url_type === 1 || url_type === '1') {
+      return `/Portal/HsArticle/index/id/${url}.html`
+    }
+    if (url_type === 2 || url_type === '2') {
+      return `/Portal/HsArticle/culture/id/${url}.html`
+    }
+    if (url_type === 3 || url_type === '3') {
+      return `/HsProject/index/pid/${url}.html`
+    }
+    if (url_type === 5 || url_type === '5') {
+      return url;
+    }
+    if (url_type === 6 || url_type === '6') {
+      return `/HsCategories/category_index/id/${url}.html`
+    }
+
+
+  }
+
+  //  神策埋点事件
+  sensorsEvent();
+  function sensorsEvent() {
+    $(page).find('.search_btn').on('click', function () {
+      init.sensors.track('buttonClick', {
+        pageType: '分类页',
+        buttonName: '搜索',
+      })
+    });
+    init.sensorsFun.bottomNav();
+
+    //分类 分类列表页
+    $(page).find('.classify_two').on('click', 'a', function () {
+      var $this = $(this);
+      var $li = $this.parents('li');
+      var url = $this.attr('href');
+      var index = $li.index();
+      var title = $this.find('span').html();
+
+      init.sensorsFun.mkt('分类', '分类列表页', title, index, '', '');
+    });
+
+    //标签 标签列表页
+    $(page).find('.label_content .tags_list').on('click', 'a', function () {
+      var $this = $(this);
+      var $li = $this;
+      var url = $this.attr('href');
+      var index = $li.index();
+      var title = init.sensorsFun.getUrlKey(url, 'tag');
+
+      init.sensorsFun.mkt('标签', '标签列表页', title, index, '', '');
+    });
+
+    //黑市书摊 标签列表页
+    $(page).find('.label_content').children('a').on('click', function () {
+      var $this = $(this);
+      var url = $this.attr('href');
+      init.sensorsFun.mkt('黑市书摊', '标签列表页', url, '', '', '');
+    });
+
+  }
 
 
 
--- app/modules/components/categories/categories_good.js
+++ app/modules/components/categories/categories_good.js
@@ -15,12 +15,14 @@ $(document).on('pageInit','.categories_good', function(e, id, page){
     link: window.location.href,
     img: $('.categories_icon').val()
   };
+  var ApiBaseUrl = init.getApiBaseUrl();
   init.wx_share(share_data);
   init.checkfollow();
 
   // 搜索初始
   // SearchInit();
   // 初始化title
+  $('.goods_filter').find('div').eq(0).addClass('active')
   $('title').text($('.categories_name').val());
   // 是否显示查看下拉
   var length = $('.categories_list_row').find('.categories_item').length - 1;
@@ -40,7 +42,7 @@ $(document).on('pageInit','.categories_good', function(e, id, page){
     $('.categories_content').css('display', 'none');
     $('.categories_list_row').css('display', 'block');
   })
-  // 分类切换
+  // 分类切换 以下这个函数前端已经注释
   $('.categories_item').click(function(){
     var status = $(this).hasClass('categories_item_all');
     $('.categories_item').removeClass('active');
@@ -59,23 +61,40 @@ $(document).on('pageInit','.categories_good', function(e, id, page){
     }else{
       tagName = '';
     }
-    reset_load(categoryID, tagName, curPage);
+    reset_load(categoryID, tagName, curPage,order_types[0]);
   })
 
-  // 标签切换
-  $('.tag_list').on('click', '.tag_item',function(){
-    $('.tag_item').removeClass('active');
-    $(this).addClass('active');
-    tagName = $('.tag_list').find('.active').attr('data-name');
-    reset_load(categoryID, tagName, curPage);
+  // 标签切换 以下这个函数前端已经注释
+  $('.tags_list_new').on('click', '.tag_item',function(){
+    let tagid = $(this).attr('data-id');
+    location.href=`${location.origin}/HsCategories/category_index/id/${tagid}.html`
+    
 
   })
+
+  //筛选切换 v4.5新增
+  var orderTypeIndex=0;
+  $('.goods_filter').on('click', 'div',function(){
+    if($(this).index()==3){
+      orderTypeIndex=orderTypeIndex==3?4:3;
+      if($(this).find('span').hasClass('active')){
+        $(this).find('span').removeClass('active')
+      }else{
+        $(this).find('span').addClass('active')      
+      }
+      $('.goods_filter div').removeClass('active');
+      $(this).addClass('active');
+
+    }else{
+      orderTypeIndex=$(this).index()
+      $('.goods_filter div').removeClass('active');
+      $(this).addClass('active');
+    }
+    reset_load(categoryID, tagName, curPage,order_types[orderTypeIndex]);
+
+  })
+
   var categories_index_tpl = handlebars.compile($("#categories_index_tpl").html());
-  //初始化内容
-  $('.goods_content').html('');
-  $('.categories_item').removeClass('active');
-  $('.categories_item_all').addClass('active');
- 
 
   // 下拉加载
   var nowCategoryID = '';
@@ -93,20 +112,53 @@ $(document).on('pageInit','.categories_good', function(e, id, page){
   var curPage = 1;
   var loading = false;
   var add_goods_status = false;
+  var order_types = ["sort_by_zonghe","sort_by_xiaoliang","post_modified","sort_by_jiage_di","sort_by_jiage_gao" ]
   // 第一次以当前页id查询
-  add_goods(categoryID, tagName, curPage);
-  function add_goods(category_id, tag, cur_page){
+  add_goods(categoryID, tagName, curPage,order_types[0]);
+  function add_goods(category_id, tag, cur_page,order_type){
+    let url =   ApiBaseUrl + '/appv6_4/category/posts';
     $.ajax({
       type: 'GET',
-      url: '/index.php?g=restful&m=HsCategories&a=ajax_posts',
+      // url: '/index.php?g=restful&m=HsCategories&a=ajax_posts',
+      url: url,
       data: {
         category_id: category_id,
-        tag: tag,
-        cur_page: cur_page
+        // tag: tag,
+        page_size:10,
+        cur_page: cur_page,
+        order: order_type 
       },
       success: function(data){
         if(data.status == 1){
           $('.goods_content').append(categories_index_tpl(data.data));
+          if(data.data.children_categories.length){
+            $('.tags_list_new').show()
+            let tagsDom = ''
+            $('.tags_list_new').html('')
+            data.data.children_categories.forEach(v=>{
+              tagsDom+=`<span class="tag_item" data-id="${v.id}" data-name="${v.name}" style="background: url(${v.icon}) #000;">${v.name}</span>`
+            })
+            console.log(tagsDom)
+            $('.tags_list_new').html(tagsDom)
+            $('.goods_filter').css('top','1.6rem')
+            $('.goods_content').css('margin-top','3rem')
+          }
+          if(data.data.ad[cur_page-1]){
+            $('.goods_content').append(
+              `
+              <div class="ad" 
+              data-type='${data.data.ad[cur_page-1].url_type}'
+              data-url='${data.data.ad[cur_page-1].url}'
+              style="background:url(${data.data.ad[cur_page-1].img}) no-repeat center center;
+              background-size:100% 100%;
+              height: 2.9rem;
+              width:9.2rem;
+              margin:0.7rem auto 0.18rem;
+              border-radius:0.16rem;
+              ">
+              `
+            )
+          }
           if(nowCategoryID && !tag){
             if(data.data.my_tag && data.data.my_tag.length > 0){
               $('.tag_list').css('height', '1.36rem');
@@ -118,7 +170,7 @@ $(document).on('pageInit','.categories_good', function(e, id, page){
             }
           }
           $('.infinite-scroll-preloader').css('display', 'block');
-          if(data.data.posts.length < 5){
+          if(data.data.item_list.result.length < 5){
             $('.infinite-scroll-preloader').css('display', 'none');
           }
         } else {
@@ -145,20 +197,50 @@ $(document).on('pageInit','.categories_good', function(e, id, page){
       // 设置flag
       loading = true;
       curPage = curPage + 1;
-      add_goods(categoryID, tagName, curPage);
+      add_goods(categoryID, tagName, curPage,order_types[orderTypeIndex]);
       $.refreshScroller();
     }
   });
-  function reset_load(categoryID, tagName, curPage){
+  function reset_load(categoryID, tagName, curPage,order_type){
     curPage = 1;
     loading = true;
     add_goods_status = false;
     $('.goods_content').html('');
     $('.infinite-scroll-preloader').css('display', 'none');
-    add_goods(categoryID, tagName, curPage);
+    add_goods(categoryID, tagName, curPage,order_type);
   }
 
+  $('.goods_content').on('click','.ad',function(){
+      var type = $(this).attr('data-type') 
+      var url = $(this).attr('data-url') 
+      console.log(type)
+      switch(type){
+        case "1":
+        location.href=`/Portal/HsArticle/index/id/${url}.html`
+          break;
+        case "2":
+        location.href=`/Portal/HsArticle/culture/id/${url}.html`
+          break;
+        case "3":
+        location.href=`/HsProject/index/pid/${url}.html`
+          break;
+        case "4":
+        location.href=`/HsProject/index/pid/${url}.html`
+          break;
+        case "5":
+        location.href=`${url}`
+          break;
+        case "6":
+        location.href=`/HsCategories/category_index/id/${url}.html`
+          break;
+        case "15":
+        location.href=`/Portal/Index/category_top10/id/${url}.html`
+          break;
+        default:
+        break;
+      }
 
+  })
   $(".content").on('scroll',function(){
     if($('.content').scrollTop() > 1000){
       $('.return_top').css('display', 'block');
